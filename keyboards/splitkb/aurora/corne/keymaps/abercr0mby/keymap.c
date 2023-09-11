8240ccc754e672b83d5c27ac2a4e841c724c191d
Paul Williams <abercromby@gmail.com>
2023-09-11 18:45:13 +0100

    Added comment

--- keyboards/splitkb/aurora/corne/keymaps/abercr0mby/keymap.c
+++ keyboards/splitkb/aurora/corne/keymaps/abercr0mby/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                             KC_TRNS, LCTL(KC_A), KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS,       KC_HOME, KC_LEFT, KC_DOWN, KC_RIGHT, KC_END, KC_TRNS,
                             KC_TRNS, LCTL(KC_Z), LCTL(KC_X), LCTL(KC_C), LCTL(KC_V), KC_TRNS,      KC_PGDN, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, TO(_DEFAULT)),
- 	[_REFERENCE] = LAYOUT_split_3x6_3(KC_NO, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_NO,
+ 	[_REFERENCE] = LAYOUT_split_3x6_3(KC_NO, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_NO,  /*Use to allow combos to use same keys and apply to all layers even if keys different*/
                             KC_NO, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_QUOT, KC_NO,
                             KC_NO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_NO,
                             KC_F1, KC_F2, KC_ENT, LALT_T(KC_SPC), KC_F11, KC_F10)
