6f98c99e409e72d040112160f0b1b1bd753c7784
Paul Williams <abercromby@gmail.com>
2023-09-12 00:04:34 +0100

    tweaking and changing

--- keyboards/splitkb/aurora/corne/keymaps/abercr0mby/keymap.c
+++ keyboards/splitkb/aurora/corne/keymaps/abercr0mby/keymap.c
@@ -27,19 +27,51 @@ void keyboard_pre_init_user(void) {
    return layer_move(state, _LOWER, _RAISE, _ADJUST);
 } */
 
+
+uint16_t get_quick_tap_term(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case LSFT_T(KC_SPC):
+            return 0;
+        default:
+            return QUICK_TAP_TERM;
+    }
+}
+
+bool get_retro_tapping(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case LSFT_T(KC_SPC):
+            return true;
+        default:
+            return false;
+    }
+}
+
+bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case LSFT_T(KC_SPC):
+            // Immediately select the hold action when another key is pressed.
+            return true;
+        default:
+            // Do not select the hold action when another key is pressed.
+            return false;
+    }
+}
+
+
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_DEFAULT] = LAYOUT_split_3x6_3(KC_NO, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_NO,
                             KC_NO, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_QUOT, KC_NO,
                             KC_NO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_NO,
-                            TO(_LOWER), RCTL_T(KC_TAB), KC_ENT, LALT_T(KC_BSPC), LSFT_T(KC_SPC), TO(_RAISE)),
+                            TO(_LOWER), KC_BSPC, LCTL_T(KC_TAB), KC_ENT, LSFT_T(KC_SPC), TO(_RAISE)),
 	[_ADJUST] = LAYOUT_split_3x6_3(KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_TRNS,
                             KC_TRNS, KC_PLUS, KC_MINUS, KC_SLASH, KC_ASTR, KC_EQUAL, KC_PERC, KC_COLN, KC_DOT, KC_F11, KC_F12, KC_TRNS,
                             KC_TRNS, KC_GRV, KC_PIPE, LSFT(KC_NUBS), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                            TO(_DEFAULT), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, TO(_DEFAULT)),
+                            TO(_DEFAULT),  KC_DEL, KC_TRNS, KC_TRNS, KC_TRNS, TO(_DEFAULT)),
     [_LOWER] = LAYOUT_split_3x6_3(KC_TRNS, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TRNS,
                             KC_TRNS, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_TRNS,
                             KC_TRNS, KC_NUHS, KC_MINUS, KC_UNDS, KC_LCBR, KC_LBRC, KC_RBRC, KC_RCBR, KC_COLN, KC_SCLN, KC_NUBS, KC_TRNS,
-                            TO(_DEFAULT), KC_TRNS, KC_TRNS, KC_DEL, KC_TRNS, KC_TRNS),
+                            TO(_DEFAULT), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 	[_RAISE] = LAYOUT_split_3x6_3(KC_TRNS, KC_ESC, KC_BTN1, KC_MS_U, KC_BTN2, KC_TRNS,      KC_PGUP, KC_DEL, KC_UP, KC_BSPC, LCTL(LALT(KC_DEL)), KC_TRNS,
                             KC_TRNS, LCTL(KC_A), KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS,       KC_HOME, KC_LEFT, KC_DOWN, KC_RIGHT, KC_END, KC_TRNS,
                             KC_TRNS, LCTL(KC_Z), LCTL(KC_X), LCTL(KC_C), LCTL(KC_V), KC_TRNS,      KC_PGDN, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
