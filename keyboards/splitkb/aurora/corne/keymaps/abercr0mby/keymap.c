ea73b52d7f12082d208cd31e63ccb2064aedf5df
Paul Williams <abercromby@gmail.com>
2023-09-13 00:09:45 +0100

    messing about with layers

--- keyboards/splitkb/aurora/corne/keymaps/abercr0mby/keymap.c
+++ keyboards/splitkb/aurora/corne/keymaps/abercr0mby/keymap.c
@@ -58,28 +58,27 @@ bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
 }
 
 
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_DEFAULT] = LAYOUT_split_3x6_3(KC_NO, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_NO,
                             KC_NO, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_QUOT, KC_NO,
                             KC_NO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_NO,
-                            TO(_LOWER), KC_BSPC, LCTL_T(KC_TAB), KC_ENT, LSFT_T(KC_SPC), TO(_RAISE)),
+                            TT(_LOWER), LSFT_T(KC_BSPC), LCTL_T(KC_TAB), KC_ENT, LCTL_T(KC_SPC), TT(_RAISE)),
 	[_ADJUST] = LAYOUT_split_3x6_3(KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_TRNS,
                             KC_TRNS, KC_PLUS, KC_MINUS, KC_SLASH, KC_ASTR, KC_EQUAL, KC_PERC, KC_COLN, KC_DOT, KC_F11, KC_F12, KC_TRNS,
                             KC_TRNS, KC_GRV, KC_PIPE, LSFT(KC_NUBS), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                            TO(_DEFAULT),  KC_DEL, KC_TRNS, KC_TRNS, KC_TRNS, TO(_DEFAULT)),
+                            TO(_DEFAULT),  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, TO(_DEFAULT)),
     [_LOWER] = LAYOUT_split_3x6_3(KC_TRNS, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TRNS,
                             KC_TRNS, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_TRNS,
                             KC_TRNS, KC_NUHS, KC_MINUS, KC_UNDS, KC_LCBR, KC_LBRC, KC_RBRC, KC_RCBR, KC_COLN, KC_SCLN, KC_NUBS, KC_TRNS,
                             TO(_DEFAULT), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
-	[_RAISE] = LAYOUT_split_3x6_3(KC_TRNS, KC_ESC, KC_BTN1, KC_MS_U, KC_BTN2, KC_TRNS,      KC_PGUP, KC_DEL, KC_UP, KC_BSPC, LCTL(LALT(KC_DEL)), KC_TRNS,
+	[_RAISE] = LAYOUT_split_3x6_3(KC_TRNS, KC_ESC, KC_BTN1, KC_MS_U, KC_BTN2, KC_TRNS,      KC_PGUP, KC_TRNS, KC_UP, KC_TRNS, LCTL(LALT(KC_DEL)), KC_TRNS,
                             KC_TRNS, LCTL(KC_A), KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS,       KC_HOME, KC_LEFT, KC_DOWN, KC_RIGHT, KC_END, KC_TRNS,
                             KC_TRNS, LCTL(KC_Z), LCTL(KC_X), LCTL(KC_C), LCTL(KC_V), KC_TRNS,      KC_PGDN, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, TO(_DEFAULT)),
  	[_REFERENCE] = LAYOUT_split_3x6_3(KC_NO, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_NO,  /*Use to allow combos to use same keys and apply to all layers even if keys different*/
                             KC_NO, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_QUOT, KC_NO,
                             KC_NO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_NO,
-                            KC_F1, KC_F2, KC_ENT, LALT_T(KC_SPC), KC_F11, KC_F10)
+                            KC_F1, KC_F2, KC_F3, LALT_T(KC_SPC), KC_F11, KC_F10)
 
 };
 
@@ -87,14 +86,21 @@ const uint16_t PROGMEM adjust_layer[] = {KC_F1, KC_F10, COMBO_END};
 const uint16_t PROGMEM default_layer[] = {KC_F2, KC_F11, COMBO_END};
 const uint16_t PROGMEM df_alt[] = {KC_F, KC_D, COMBO_END};
 const uint16_t PROGMEM jk_win[] = {KC_J, KC_K, COMBO_END};
-const uint16_t PROGMEM as_esc[] = {KC_A, KC_S, COMBO_END};
+const uint16_t PROGMEM er_esc[] = {KC_E, KC_R, COMBO_END};
+const uint16_t PROGMEM f_bkspc_del[] = {KC_F, KC_F2, COMBO_END};
+//const uint16_t PROGMEM f_shift_ctrl[] = {KC_F, KC_F2, COMBO_END};
+const uint16_t PROGMEM j_ctrl_shift[] = {KC_J, KC_F11, COMBO_END};
+
 
 combo_t key_combos[] = {
     COMBO(adjust_layer, TO(_ADJUST)),
     COMBO(default_layer, TO(_DEFAULT)),
     COMBO(df_alt,  KC_LALT),
     COMBO(jk_win,  KC_LGUI),
-    COMBO(as_esc,  KC_ESC)
+    COMBO(er_esc,  KC_ESC),
+    COMBO(f_bkspc_del,  KC_DEL),
+    //COMBO(f_shift_ctrl,  KC_LCTL),
+    COMBO(j_ctrl_shift,  KC_LSFT)
 };
 
 #if defined(ENCODER_ENABLE) && defined(ENCODER_MAP_ENABLE)
