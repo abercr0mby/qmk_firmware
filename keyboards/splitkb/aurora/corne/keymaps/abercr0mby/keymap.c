ba70c22a19effa258c41f0098f34b4e17b516f4b
Paul Williams <abercromby@gmail.com>
2023-09-26 13:23:57 +0100

    Mostly nav improvements

--- keyboards/splitkb/aurora/corne/keymaps/abercr0mby/keymap.c
+++ keyboards/splitkb/aurora/corne/keymaps/abercr0mby/keymap.c
@@ -32,45 +32,45 @@ void keyboard_pre_init_user(void) {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_DEFAULT] = LAYOUT_split_3x6_3(
-        KC_NO, KC_Q, KC_W, KC_E, KC_R, KC_T,                                    KC_Y, KC_U, KC_I, KC_O, KC_P, KC_NO,
-        KC_NO, KC_A, LSFT_T(KC_S), LCTL_T(KC_D), LALT_T(KC_F), LGUI_T(KC_G),    KC_H, KC_J, KC_K, KC_L, KC_QUOT, KC_NO,
-        KC_NO, KC_Z, KC_X, KC_C, KC_V, KC_B,                                    KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_NO,
+        KC_NO, KC_Q, KC_W, KC_E, KC_R, KC_T,                                            KC_Y, KC_U, KC_I, KC_O, KC_P, KC_NO,
+        KC_NO, KC_A, LSFT_T(KC_S), LCTL_T(KC_D), LALT_T(KC_F), LGUI_T(KC_G),            KC_H, KC_J, KC_K, KC_L, KC_QUOT, KC_NO,
+        KC_NO, KC_Z, KC_X, KC_C, KC_V, KC_B,                                            KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_NO,
         LCTL_T(KC_TAB), TT(_SYMBOL), LSFT_T(KC_BSPC), KC_SPC, TT(_NAV), KC_ENT),
 
 	[_ADJUST] = LAYOUT_split_3x6_3(
-        KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_LALT,                           KC_NUHS, KC_HASH, KC_DLR, KC_CIRC, KC_AMPR, KC_TRNS,
-        KC_TRNS, KC_F5, KC_F6, KC_F7, KC_F8, KC_TRNS,                           KC_GRV, KC_PIPE, LSFT(KC_NUBS), KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS,  KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS,                       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_LALT,                                   KC_NUHS, KC_HASH, KC_DLR, KC_CIRC, KC_AMPR, KC_TRNS,
+        KC_TRNS, KC_F5, KC_F6, KC_F7, KC_F8, KC_TRNS,                                   KC_GRV, KC_PIPE, LSFT(KC_NUBS), KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS,  KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS,                               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, TO(_DEFAULT), KC_TRNS, KC_TRNS, TO(_MOUSE), KC_TRNS),
 
     [_SYMBOL] = LAYOUT_split_3x6_3(
         KC_TRNS, KC_PLUS, KC_1, KC_2, KC_3, KC_MINUS,                                   KC_LPRN, KC_RPRN, KC_EXLM, KC_AT, KC_UNDS, KC_TRNS,
-        KC_TRNS, KC_SLASH, LSFT_T(KC_4), LCTL_T(KC_5), LALT_T(KC_6), LGUI_T(KC_ASTR),   KC_LCBR, KC_RCBR, KC_PERC, KC_SCLN, KC_COLN, KC_TRNS,
+        KC_TRNS, KC_SLASH, LSFT_T(KC_4), LCTL_T(KC_5), LALT_T(KC_6), KC_ASTR,           KC_LCBR, KC_RCBR, KC_PERC, KC_SCLN, KC_COLN, KC_TRNS,
         KC_TRNS, KC_EQUAL, KC_7, KC_8, KC_9, KC_0,                                      KC_LBRC, KC_RBRC, KC_TRNS, KC_TRNS, KC_NUBS, KC_TRNS,
         KC_TRNS, TO(_DEFAULT), KC_TRNS, KC_TRNS, TO(_NAV), KC_TRNS),
 
 	[_NAV] = LAYOUT_split_3x6_3(
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,               KC_PGUP, KC_MS_BTN1, KC_UP, KC_MS_BTN2, LCTL(LALT(KC_DEL)), KC_TRNS,
-        KC_TRNS, LCTL(KC_A), KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,            KC_HOME, KC_LEFT, KC_DOWN, KC_RIGHT, KC_END, KC_TRNS,
-        KC_TRNS, LCTL(KC_Z), LCTL(KC_X), LCTL(KC_C), LCTL(KC_V), KC_TRNS,   KC_PGDN, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                           KC_PGUP, LCTL(KC_LEFT), KC_UP, LCTL(KC_RIGHT), LCTL(LALT(KC_DEL)), KC_TRNS,
+        KC_TRNS, LCTL(KC_A), KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,                        KC_HOME, KC_LEFT, KC_DOWN, KC_RIGHT, KC_END, KC_TRNS,
+        KC_TRNS, LCTL(KC_Z), LCTL(KC_X), LCTL(KC_C), LCTL(KC_V), KC_TRNS,               KC_PGDN, KC_APP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS,  TO(_SYMBOL), KC_TRNS, KC_TRNS, TO(_DEFAULT), KC_TRNS),
 
     [_MOUSE] = LAYOUT_split_3x6_3(
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,               KC_TRNS, KC_BTN1, KC_MS_U, KC_BTN2, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,               KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                           KC_TRNS, KC_BTN1, KC_MS_U, KC_BTN2, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                           KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, TO(_DEFAULT), KC_TRNS, KC_TRNS, TO(_GAME), KC_TRNS),
 
     [_GAME] = LAYOUT_split_3x6_3(
-        KC_TRNS, KC_1, KC_2, KC_3, KC_4, KC_5,                     KC_6, KC_7, KC_8, KC_9, KC_0, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_Q, KC_W, KC_E, KC_R,         KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_LSFT, KC_A, KC_S, KC_D, KC_F,               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_1, KC_2, KC_3, KC_4, KC_5,                                          KC_6, KC_7, KC_8, KC_9, KC_0, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_Q, KC_W, KC_E, KC_R,                                       KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_LSFT, KC_A, KC_S, KC_D, KC_F,                                       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TAB, KC_LCTL, KC_SPC,     KC_LALT, TO(_DEFAULT), KC_TRNS),
 
  	[_REFERENCE] = LAYOUT_split_3x6_3(
-        KC_NO, KC_Q, KC_W, KC_E, KC_R, KC_T,                                KC_Y, KC_U, KC_I, KC_O, KC_P, KC_NO,  /*Use to allow combos to use same keys and apply to all layers even if keys different*/
-        KC_NO, KC_A, KC_S, KC_D, KC_F, KC_G,                                KC_H, KC_J, KC_K, KC_L, KC_QUOT, KC_NO,
-        KC_NO, KC_Z, KC_X, KC_C, KC_V, KC_B,                                KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_NO,
+        KC_NO, KC_Q, KC_W, KC_E, KC_R, KC_T,                                            KC_Y, KC_U, KC_I, KC_O, KC_P, KC_NO,  /*Use to allow combos to use same keys and apply to all layers even if keys different*/
+        KC_NO, KC_A, KC_S, KC_D, KC_F, KC_G,                                            KC_H, KC_J, KC_K, KC_L, KC_QUOT, KC_NO,
+        KC_NO, KC_Z, KC_X, KC_C, KC_V, KC_B,                                            KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_NO,
         KC_F1, KC_F2, KC_F3, KC_F12, KC_F11, KC_F10)
 };
 
@@ -89,7 +89,7 @@ combo_t key_combos[] = {
 #if defined(ENCODER_ENABLE) && defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
 
-};
+};g
 #endif // defined(ENCODER_ENABLE) && defined(ENCODER_MAP_ENABLE)
 
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
@@ -105,6 +105,7 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     }
 }
 
+
 bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case LSFT_T(KC_BSPC):
@@ -119,7 +120,10 @@ bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
     }
 }
 
-uint16_t get_quick_tap_term(uint16_t keycode, keyrecord_t *record) {
+/*This allows the hold instead of the tap when held immediatley after a tap. If 0 you get the hold, otherwise you can get the
+multi-tap, the effect you would normally get when holding the key. NOTE this seems to break the layer toggle part of TT so
+need to decide if it is worth it. In theory should not be tapping layer keys as they stand as they are dedicated at time of writing.*/
+/*uint16_t get_quick_tap_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case TT(_NAV):
         case TT(_SYMBOL):
@@ -127,7 +131,7 @@ uint16_t get_quick_tap_term(uint16_t keycode, keyrecord_t *record) {
         default:
             return QUICK_TAP_TERM;
     }
-}
+}*/
 
 /* bool get_retro_tapping(uint16_t keycode, keyrecord_t *record) { //if no keypress follows it counts as a tap even if held long enough
     switch (keycode) {
